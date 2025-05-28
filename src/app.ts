efd19ecde0a791bc06459dbb04c567244b1f1e68
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-05-28 10:08:55 +0200

    Add autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFr = originalMethod.bind(this);
+      return boundFr;
+    },
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;

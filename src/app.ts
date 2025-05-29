62e52e442b38f223983fa97d66c549e631566364
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-05-29 11:44:42 +0200

    Add move functionality

--- src/app.ts
+++ src/app.ts
@@ -62,6 +62,16 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active
     );
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+  moveProject(projectID: String, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectID);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -234,9 +244,13 @@ class ProjectList
       listEl.classList.add("droppable");
     }
   }
-
+  @autobind
   dropHandler(event: DragEvent) {
-    console.log(event.dataTransfer!.getData("text/plain"));
+    const prjId = event.dataTransfer!.getData("text/plain");
+    projectState.moveProject(
+      prjId,
+      this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+    );
   }
 
   @autobind

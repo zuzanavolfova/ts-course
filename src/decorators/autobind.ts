1ec01e055698a6a1f892c44b72b79f579c7bccb2
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-05-30 15:37:59 +0200

    Add folders for ts, make separate files for functionality

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+namespace App {
+  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFr = originalMethod.bind(this);
+        return boundFr;
+      },
+    };
+    return adjDescriptor;
+  }
+}
